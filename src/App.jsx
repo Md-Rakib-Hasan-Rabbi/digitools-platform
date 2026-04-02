2bed40d81b653c3de7bf5dcf27de6cf1e3901002
Rakib <rakibmcpsc339@gmail.com>
2026-04-03 00:52:28 +0600

    Necessary resources added

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 class="text-6xl font-bold underline border-blue-500 ">Hello there</h1>
+      <button class="btn btn-primary">Click me</button>
+    </>
+  )
+}
+
+export default App
